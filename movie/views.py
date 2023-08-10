f7c0181bfc6505a54c3d81b959e3c0fefcdb26a9
Daniela Alvarez <dalvareza99@gmail.com>
2023-08-10 00:21:31 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Daniela Álvarez'})
+   # return render(request, 'home.html', {'name': 'Daniela Álvarez'})
+    searchTerm=request.GET.get('searchMovie')
+    movies=Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm:': searchTerm,'movies':movies})
+
 def about(request):
-    return render(request, 'about.html')
+
+    return HttpResponse('<h1> Welcome to About Page </h1>')
