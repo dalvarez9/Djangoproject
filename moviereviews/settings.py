62e52c87d3525b2c7a505dfed36c130c4f50a06d
Daniela Alvarez <dalvareza99@gmail.com>
2023-08-23 18:52:47 -0500

    News, carrusel

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -17,15 +17,14 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-MEDIA_URL='/media/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fm0u=2sz4!rpym%qbc&)o36x*&9usjk6^pg79ugt)-s&d+x$#%'
+SECRET_KEY = 'django-insecure-bk7qd*_qi)l$s&xh2oo$1=b@z-zqh*dwo@u+n=y7-#6(lhu_p4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movie'
+    'movie',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'moviereviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'moviereviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,6 +71,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'moviereviews/static/',
 ]
 
 WSGI_APPLICATION = 'moviereviews.wsgi.application'
